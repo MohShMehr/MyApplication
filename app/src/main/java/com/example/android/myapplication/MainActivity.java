883a38061f4f10e5a928d61793fa769afd5ef8c1
moh.sh.mehr1368@gmail.com <moh.sh.mehr1368@gmail.com>
2017-08-14 10:49:41 +0430

    changing the refrence

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.android.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textview);
     }
 }
