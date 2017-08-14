73ed52661d7f19ea8061617574c20f253fbef52b
moh.sh.mehr1368@gmail.com <moh.sh.mehr1368@gmail.com>
2017-08-14 08:42:18 +0430

    new refrence

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.android.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
